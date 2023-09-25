2cd189bfaf148118ca0cddeb30219649ffeb68d5
iz037 <119796971+iz037@users.noreply.github.com>
2023-09-25 10:52:46 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-setclipboard("https://discord.gg/DMjPDbuUgQ")
+setclipboard("https://discord.gg/FJFZMCu5JX")
 repeat wait() until game:IsLoaded()
 
 if game.PlaceId == 3398014311 then
@@ -15,6 +15,8 @@ elseif game.PlaceId == 13772394625 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/iz037/Zeld-Hub/main/Script/Blade%20Ball.lua"))()
 elseif game.PlaceId == 14732610803 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/iz037/Zeld-Hub/main/Script/Blade%20Ball.lua"))()
+elseif game.PlaceId == 14795754298 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/iz037/Zeld-Hub/main/Script/The%20Russian%20Sleep%20Experiment.lua"))()
 else
 
     game:GetService("StarterGui"):SetCore("SendNotification",
